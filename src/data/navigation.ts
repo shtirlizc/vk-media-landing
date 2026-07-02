1f8c8545c3a5b83e198f4839081e838895edc835
shtirlizc <shtirlizc@gmail.com>
2026-07-02 14:39:17 +0500

    page structure

---
+++ src/data/navigation.ts
@@ -0,0 +1,14 @@
+export const navigation = [
+  {
+    label: "Бизнес-группа",
+    href: "business-group",
+  },
+  {
+    label: "Команда",
+    href: "team",
+  },
+  {
+    label: "Полезное",
+    href: "useful",
+  },
+];
