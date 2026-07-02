f3db72b71761f833af1be6dc2ced2c1db6183425
shtirlizc <shtirlizc@gmail.com>
2026-07-02 16:07:07 +0500

    add site and base to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shtirlizc.github.io',
+  base: '/vk-media-landing',
   fonts: [
     {
       provider: fontProviders.local(),
