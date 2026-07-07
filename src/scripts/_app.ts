12b2af2359332c1ba51655a4c53b7b40b1227b37
Mart Nemov <mr.nemov@gmail.com>
2026-07-07 17:54:52 +0500

    Add video playback modal to `team` block: integrate new `initPeople` script, update styles, markup, and assets

--- src/scripts/_app.ts
+++ src/scripts/_app.ts
@@ -2,8 +2,10 @@ import { initHeader } from "./header.ts";
 import { initVideos } from "./video.ts";
 import { initGroup } from "./group.ts";
 import { moreThan } from "./more-than.ts";
+import { initPeople } from "./people.ts";
 
 initHeader();
 initVideos();
 initGroup();
 moreThan();
+initPeople();
